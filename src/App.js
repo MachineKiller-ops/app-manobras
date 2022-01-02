e3679e6dd9dc256cc8fb930fdd5bb9932d2b3339
Caio Cesar <caiocesaraguilar14@hotmail.com>
2022-01-02 19:04:07 -0300

    adiciona barra de navegação

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import Nav from './Pages/Nav';
+
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Helloo World</h1>
-      </header>
-    </div>
+    <div>
+
+                <Nav user={user} />
+
+            </div>
   );
 }
 
---
+++ src/Pages/Nav.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../nav.css'
+import { Link } from 'react-router-dom';
+
+const Nav = (props) => {
+
+    const navStyle = {
+        color: 'white'
+    };
+    return (
+        <nav>
+            <h1>CEMIG DISTRIBUIÇÃO</h1>
+            <ul className="nav-links">
+                <Link style={navStyle} to="/">
+                    <li>Home</li>
+                </Link>
+                <Link style={navStyle} to="/elabora">
+                    <li>Elaborar</li>
+                </Link>
+                <Link style={navStyle} to="/simula">
+                    <li>Simular</li>
+                </Link>
+                <Link style={navStyle} to="/configurase">
+                    <li>Configurar</li>
+                </Link>
+
+
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
